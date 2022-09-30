9e5815c08672eb43e4b102a37bc9cf3980e2dc7d
mdrokz <mohammadmunshi@gmail.com>
2022-09-30 20:28:24 +0530

    change file name

--- arochem/fetch.ts
+++ arochem/fetch.ts
@@ -1,9 +1,9 @@
-import {get} from 'http';
+import {get,} from 'https';
 
 
-export function fetch(url: `https://${string}.com`): Promise<string> {
+export default function fetch(url: `${string}.com`): Promise<string> {
     return new Promise((resolve, reject) => {
-        get(url, (res) => {
+        get({host: 'https',hostname: url, headers: {['User-Agent']: 'Mozilla/5.0 (X11; Linux armv7l) AppleWebKit/537.36 (KHTML, like Gecko) Raspbian Chromium/74.0.3729.157 Chrome/74.0.3729.157 Safari/537.36'}}, (res) => {
             let data = '';
             res.on('data', (chunk) => {
                 data += chunk;
