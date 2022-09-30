d633f0a557e4ff5b87bf3c9e398c855e1f37b514
mdrokz <mohammadmunshi@gmail.com>
2022-09-30 21:17:06 +0530

    parse url for get call

--- arochem/fetch.ts
+++ arochem/fetch.ts
@@ -1,9 +1,11 @@
-import {get,} from 'https';
+import { get } from 'https';
+import { parse } from 'url';
 
 
-export default function fetch(url: `${string}.com`): Promise<string> {
+export default function fetch(url: `https://${string}.com` | `https://${string}.com${string}`): Promise<string> {
     return new Promise((resolve, reject) => {
-        get({host: 'https',hostname: url, headers: {['User-Agent']: 'Mozilla/5.0 (X11; Linux armv7l) AppleWebKit/537.36 (KHTML, like Gecko) Raspbian Chromium/74.0.3729.157 Chrome/74.0.3729.157 Safari/537.36'}}, (res) => {
+        const { port, hostname, pathname } = parse(url);
+        get({ port: port, host: hostname, path: pathname, headers: { ['User-Agent']: 'Mozilla/5.0 (X11; Linux armv7l) AppleWebKit/537.36 (KHTML, like Gecko) Raspbian Chromium/74.0.3729.157 Chrome/74.0.3729.157 Safari/537.36' } }, (res) => {
             let data = '';
             res.on('data', (chunk) => {
                 data += chunk;
