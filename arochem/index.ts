198dc26d97e05ecd816b637f17b3bb53d826d71a
mdrokz <mohammadmunshi@gmail.com>
2022-09-30 20:28:44 +0530

    scrape section links

--- arochem/index.ts
+++ arochem/index.ts
@@ -1,5 +1,31 @@
-import { fetch } from "./http";
+import fetch from "./fetch";
 
-const BASE_URL = "https://arochem.com";
+import * as cheerio from "cheerio";
 
-const html = fetch(BASE_URL)
+const BASE_URL = "arochem.com";
+
+const html = await fetch(BASE_URL);
+
+const $ = cheerio.load(html);
+
+const attar_sections = $('.grid.grid--center');
+
+type Section = Record<string,string[]>;
+
+let section_links: Section[] = [];
+
+
+for (const sections of attar_sections[0].children) {
+    let names = $(sections).children();
+    let section_name = $(names[0]).text();
+
+    let section = [];
+    
+    for(const name of names.splice(1)) {
+        let link = $(name).children().attr('href');
+        section.push(link);
+    }
+    section_links.push({[section_name.trim()]: section});
+}
+
+console.log(section_links);
